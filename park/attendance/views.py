31c1105efba6198237fa19a26324bfdbb0b1c316
santhosh1217 <79699124+santhosh1217@users.noreply.github.com>
2022-10-15 10:31:00 +0530

    version 0.2

--- park/attendance/views.py
+++ park/attendance/views.py
@@ -7,22 +7,125 @@ def login(request):
     return render(request, "login.html")
 
 
-
-
 def dep(request):
     username = request.POST['u']
     password = request.POST['p']
     print(username+password)
 
-    if username=="park" and password=="1234":
-        data = {"msg": "invalid"}
-        return render(request, "department.html",data)
+    if username=="park" and password=="7122":
+
+        return render(request, "department.html")
 
     else:
-
-        return render(request, "login.html")
-
+        data = {"msg": "invalid"}
+        return render(request, "login.html",{"msg": "invalid"})
 
 
 def year(request):
+
     return render(request, "year.html")
+
+#cse
+def cse_first(request):
+    message = {"year": "first year","department":"computer science "}
+    return render(request,"attendance.html",message)
+
+def cse_second(request):
+    message = {"year": "second year","department":"computer science "}
+    return render(request,"attendance.html",message)
+
+def cse_third(request):
+    message = {"year": "third year","department":"computer science "}
+    return render(request,"attendance.html",message)
+
+def cse_final(request):
+    message = {"year": "final year","department":"computer science "}
+    return render(request,"attendance.html",message)
+
+#it
+def it_first(request):
+    message = {"year": "first year","department":"information technology "}
+    return render(request,"attendance.html",message)
+
+def it_second(request):
+    message = {"year": "second year","department":"information technology "}
+    return render(request,"attendance.html",message)
+
+def it_third(request):
+    message = {"year": "third year","department":"information technology"}
+    return render(request,"attendance.html",message)
+
+def it_final(request):
+    message = {"year": "final year","department":"information technology"}
+    return render(request,"attendance.html",message)
+
+#ece
+def ece_first(request):
+    message = {"year": "first year","department":"electronics and communication engineering"}
+    return render(request,"attendance.html",message)
+
+def ece_second(request):
+    message = {"year": "second year","department":"electronics and communication engineering"}
+    return render(request,"attendance.html",message)
+
+def ece_third(request):
+    message = {"year": "third year","department":"electronics and communication engineering"}
+    return render(request,"attendance.html",message)
+
+def ece_final(request):
+    message = {"year": "final year","department":"electronics and communication engineering"}
+    return render(request,"attendance.html",message)
+
+#mech
+def mech_first(request):
+    message = {"year": "first year","department":"mechanical"}
+    return render(request,"attendance.html",message)
+
+def mech_second(request):
+    message = {"year": "second year","department":"mechanical"}
+    return render(request,"attendance.html",message)
+
+def mech_third(request):
+    message = {"year": "third year","department":"mechanical"}
+    return render(request,"attendance.html",message)
+
+def mech_final(request):
+    message = {"year": "final year","department":"mechanical"}
+    return render(request,"attendance.html",message)
+
+
+def civil_first(request):
+    message = {"year": "first year","department":"civil"}
+    return render(request,"attendance.html",message)
+
+def civil_second(request):
+    message = {"year": "second year","department":"civil"}
+    return render(request,"attendance.html",message)
+
+def civil_third(request):
+    message = {"year": "third year","department":"civil"}
+    return render(request,"attendance.html",message)
+
+def civil_final(request):
+    message = {"year": "final year","department":"civil"}
+    return render(request,"attendance.html",message)
+
+
+def eee_first(request):
+    message = {"year": "first year","department":"electrical and electronics engineering"}
+    return render(request,"attendance.html",message)
+
+def eee_second(request):
+    message = {"year": "second year","department":"electrical and electronics engineering"}
+    return render(request,"attendance.html",message)
+
+def eee_third(request):
+    message = {"year": "third year","department":"electrical and electronics engineering"}
+    return render(request,"attendance.html",message)
+
+def eee_final(request):
+    message = {"year": "final year","department":"electrical and electronics engineering"}
+    return render(request,"attendance.html",message)
+
+
+
--- park/park/urls.py
+++ park/park/urls.py
@@ -21,5 +21,41 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("",views.login),
     path("department/",views.dep),
-    path("department/year/",views.year),
+
+    path("department/cse/year/",views.year),
+    path("department/it/year/",views.year),
+    path("department/ece/year/",views.year),
+    path("department/eee/year/",views.year),
+    path("department/mech/year/",views.year),
+    path("department/civil/year/",views.year),
+
+    path("department/cse/year/first/",views.cse_first),
+    path("department/cse/year/second/", views.cse_second),
+    path("department/cse/year/third/", views.cse_third),
+    path("department/cse/year/final/", views.cse_final),
+
+    path("department/ece/year/first/", views.ece_first),
+    path("department/ece/year/second/", views.ece_second),
+    path("department/ece/year/third/", views.ece_third),
+    path("department/ece/year/final/", views.ece_final),
+
+    path("department/it/year/first/", views.it_first),
+    path("department/it/year/second/", views.it_second),
+    path("department/it/year/third/", views.it_third),
+    path("department/it/year/final/", views.it_final),
+
+    path("department/mech/year/first/", views.mech_first),
+    path("department/mech/year/second/", views.mech_second),
+    path("department/mech/year/third/", views.mech_third),
+    path("department/mech/year/final/", views.mech_final),
+
+    path("department/eee/year/first/", views.eee_first),
+    path("department/eee/year/second/", views.eee_second),
+    path("department/eee/year/third/", views.eee_third),
+    path("department/eee/year/final/", views.eee_final),
+
+    path("department/civil/year/first/", views.civil_first),
+    path("department/civil/year/second/", views.civil_second),
+    path("department/civil/year/third/", views.civil_third),
+    path("department/civil/year/final/", views.civil_final),
 ]
