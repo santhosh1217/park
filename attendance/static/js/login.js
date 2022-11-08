92bc12f34d960a467dd9a3f72c61cd92a31263e0
santhosh1217 <79699124+santhosh1217@users.noreply.github.com>
2022-11-08 21:21:00 +0530

    Update login.js

--- attendance/static/js/login.js
+++ attendance/static/js/login.js
@@ -8,7 +8,7 @@ function leftClick()
 	btn.style.left='0';
 	document.getElementById("type").value = "admin";
 	document.getElementById("user").innerHTML = "admin";
-	//document.body.style.backgroundImage="url(https://s3.amazonaws.com/prod.static-content.quillette.com/2022/06/Student.png)";
+	document.body.style.backgroundImage="url(https://s3.amazonaws.com/prod.static-content.quillette.com/2022/06/Student.png)";
 	document.getElementById("signup").style.visibility = "visible";
 }
 
@@ -18,7 +18,7 @@ function rightClick()
 	btn.style.left='110px';
 	document.getElementById("type").value= "staff";
 	document.getElementById("user").innerHTML = "staff";
-	//document.body.style.backgroundImage="url(https://s3.amazonaws.com/prod.static-content.quillette.com/2022/07/Aging.png)";
+	document.body.style.backgroundImage="url(https://s3.amazonaws.com/prod.static-content.quillette.com/2022/07/Aging.png)";
 	document.getElementById("signup").style.visibility = "hidden";
 	
 }
