2a555a157cc8bb091047760eb1a4aef7ebf79015
santhosh1217 <79699124+santhosh1217@users.noreply.github.com>
2022-11-08 19:58:20 +0530

    Delete attendance directory

--- attendance/attendance/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from attendance import models
-
-# Register your models here.
-admin.site.register(models.college)
-admin.site.register(models.Staff)
-admin.site.register(models.Student)
-
